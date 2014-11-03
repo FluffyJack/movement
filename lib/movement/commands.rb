f6dbec65048e7dd64d4ead5e10ca1c35cbd3b379
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-11-04 09:07:45 +1100

    First command!
    This is the first real command for movement.
    All it does is allow you to run:
    
        $ move view hello
    
    By running that you'll get a basic UIView class file
    in the app/views directory.

--- lib/movement/commands.rb
+++ lib/movement/commands.rb
@@ -1,6 +1,18 @@
-class MyThorCommand < Thor
-  desc "foo", "Prints foo"
-  def foo
-    puts "foo"
+module Movement
+  class Commands < Thor::Group
+    include Thor::Actions
+
+    argument :generator
+    argument :name
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def generate
+      if generator == "view"
+        template("templates/UIView.tt", "app/views/#{name}.rb")
+      end
+    end
   end
 end
