d186245480c7f7a14c8a78757815e14ba6e35349
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-11-04 09:20:10 +1100

    Actually using subcommands
    Thor allows me to define subcommands, so now
    the generator can be used similar to the Rails one

--- lib/movement/commands.rb
+++ lib/movement/commands.rb
@@ -1,18 +1,7 @@
+require "movement/generate_command"
+
 module Movement
-  class Commands < Thor::Group
-    include Thor::Actions
-
-    argument :generator
-    argument :name
-
-    def self.source_root
-      File.dirname(__FILE__)
-    end
-
-    def generate
-      if generator == "view"
-        template("templates/UIView.tt", "app/views/#{name}.rb")
-      end
-    end
+  class Commands < Thor
+    register(Movement::GenerateCommand, "generate", "generate", "Generate resources for your app")
   end
 end
---
+++ lib/movement/generate_command.rb
@@ -0,0 +1,18 @@
+module Movement
+  class GenerateCommand < Thor::Group
+    include Thor::Actions
+
+    argument :generator
+    argument :name
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def generate
+      if generator == "view"
+        template("templates/UIView.tt", "app/views/#{name}.rb")
+      end
+    end
+  end
+end
