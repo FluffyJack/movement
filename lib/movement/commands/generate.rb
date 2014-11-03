82befb1793e0ca095116e4e96b5d2e0415da9f9e
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-11-04 10:55:31 +1100

    Moved view generator into it's own sub-sub-command

--- lib/movement/commands/generate.rb
+++ lib/movement/commands/generate.rb
@@ -1,21 +1,9 @@
+require "movement/commands/generators/view"
+
 module Movement
   module Commands
-    class Generate < Thor::Group
-      include Thor::Actions
-
-      argument :generator
-      argument :name
-      class_option :destination_root, default: "app"
-
-      def self.source_root
-        File.dirname(__FILE__)
-      end
-
-      def generate
-        if generator == "view"
-          template("templates/UIView.tt", "#{options[:destination_root]}/views/#{name}.rb")
-        end
-      end
+    class Generate < Thor
+      register(Generators::View, "view", "view", "Generate a view")
     end
   end
 end
--- spec/movement/commands/generators/view_spec.rb
+++ spec/movement/commands/generators/view_spec.rb
@@ -1,28 +1,28 @@
 require "helper"
-require "movement/commands/generate"
+require "movement/commands/generators/view"
 
 module Movement
   module Commands
-    describe Generate do
-      after do
-        ::FileUtils.rm_rf(destination_root)
-      end
+    module Generators
+      describe View do
+        after do
+          ::FileUtils.rm_rf(destination_root)
+        end
 
-      context "view" do
         it "creates the view file" do
-          command = Generate.new(["view", "hello"], { destination_root: destination_root })
+          command = View.new(["hello"], { destination_root: destination_root })
           capture(:stdout) { command.invoke_all }
           expect(File.exist?(File.join(destination_root, "views/hello.rb"))).to be true
         end
 
         it "writes the basic class skeleton" do
-          command = Generate.new(["view", "hello"], { destination_root: destination_root })
+          command = View.new(["hello"], { destination_root: destination_root })
           capture(:stdout) { command.invoke_all }
           expect(File.read(File.join(destination_root, "views/hello.rb"))).to include "class Hello < UIView"
         end
 
         it "can handle multi-word class/file names" do
-          command = Generate.new(["view", "hello_world"], { destination_root: destination_root })
+          command = View.new(["hello_world"], { destination_root: destination_root })
           capture(:stdout) { command.invoke_all }
           expect(File.read(File.join(destination_root, "views/hello_world.rb"))).to include "class HelloWorld < UIView"
         end
